6e54f5eebf05c863a9ef90a58e54e7d873fb297a
sexfat <bryant6831@gmail.com>
2021-07-23 11:58:04 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import 'assets/sass/style';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  h1 {
+   font-size: $h1;
+   margin-bottom: 100px;
+   color: #f20;
+  }
+}
+</style>
